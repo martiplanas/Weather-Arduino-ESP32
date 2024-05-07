b425203e8ce0ce9d821f3925c66dfe6f468b171e
Martí Planas Domenech <84787020+martiplanas@users.noreply.github.com>
2024-05-07 08:26:31 +0200

    initial

---
+++ main.ino
@@ -0,0 +1,183 @@
+#include <OpenWeather.h>
+#include <JSON_Decoder.h>
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+// Definim el pin de l'Arduino que utilitzarem per controlar el led o la matriu de leds
+#define PIN            25
+// Nยบ de leds connectats
+#define NUMPIXELS      64
+
+const char* ssid = "test";
+const char* password = "arduinop";
+
+String api_key = "9a77e9742b30c25a5d4745718be35c17"; 
+
+String latitude =  "42.9831"; 
+String longitude = "2.8249";
+
+String units = "metric"; 
+String language = "en"; 
+
+OW_Weather ow;
+int temp = 0; 
+
+int num0[] = {12, 13, 14, 22, 30, 38, 46, 54, 53, 52, 44, 36, 28, 20};
+int num1[] = {14, 13, 12, 21, 29, 37, 45, 53, 54};
+int num2[] = {12, 13, 14, 22, 30, 29, 28, 36, 44, 52, 53, 54};
+int num3[] = {12, 13, 14, 20, 28, 29, 36, 44, 52, 53, 54};
+int num4[] = {12, 20, 28, 29, 30, 38, 36, 46, 44, 54, 52};
+int num5[] = {12, 13, 14, 20, 28, 29, 30, 38, 46, 52, 53, 54};
+int num6[] = {12, 13, 14, 20, 28, 29, 30, 38, 46, 54, 53, 52, 22};
+int num7[] = {12, 20, 29, 28, 36, 44, 52, 53, 54};
+int num8[] = {12, 20, 29, 28, 36, 44, 52, 53, 54, 13, 14, 22, 30, 38, 46};
+int num9[] = {12, 20, 28, 29, 30, 38, 36, 46, 44, 52, 53, 54};
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {  
+  Serial.begin(115200);
+  pixels.begin(); // This initializes the NeoPixel library.
+  ConnectWifi();
+  pixels.show();
+  temp = round(getWeather());
+}
+
+void loop() {  
+
+  ClearDisplay();
+  Serial.print(temp);
+  DisplayNumber(13, 20);
+  delay(5 * 60 * 1000);
+  pixels.show(); // Mostrar els canvis als LEDs
+}
+
+int getWeather()
+{
+  OW_forecast  *forecast = new OW_forecast;
+
+  Serial.print("\nRequesting weather information from OpenWeather... ");
+
+  ow.getForecast(forecast, api_key, latitude, longitude, units, language);
+  Serial.print( round(forecast->temp[0]));
+  return forecast->temp[0];
+  delete forecast;
+}
+
+
+void DisplayNumber(int num, int intensity){//ONLY 0 to 99
+  int d1;
+  int d2;
+
+  //Separar numero per digits
+  if(num < 10) {
+    d1 = 0; 
+    d2 = num; 
+  } else {
+    d1 = num / 10; 
+    d2 = num % 10; 
+
+    Serial.print(d1);
+    Serial.print(d2);
+  }
+
+
+  for(int i = 0; i < 2; i++){
+    int n;
+    int m;
+
+    //prepares variables depending on loop and number position
+    if(i == 0){
+      n = d1;
+      m = 0;
+    }else if (i == 1){
+      n = d2;
+      m = -4;
+    }
+    Serial.print(n);
+    //Draw numbers
+    if (n == 0){
+      for (int i = 0; i < sizeof(num0) / sizeof(num0[0]); i++) {
+        int numberOfLef = num0[i];
+        pixels.setPixelColor(numberOfLef+m, intensity, intensity, intensity);
+      }
+    }
+    if (n == 1){
+      for (int i = 0; i < sizeof(num1) / sizeof(num1[0]); i++) {
+        int numberOfLef = num1[i];
+        pixels.setPixelColor(numberOfLef+m, intensity, intensity, intensity);
+      }
+    }
+    if (n == 2){
+      for (int i = 0; i < sizeof(num2) / sizeof(num2[0]); i++) {
+        int numberOfLef = num2[i];
+        pixels.setPixelColor(numberOfLef+m, intensity, intensity, intensity);
+      }
+    }
+    if (n == 3){
+      for (int i = 0; i < sizeof(num3) / sizeof(num3[0]); i++) {
+        int numberOfLef = num3[i];
+        pixels.setPixelColor(numberOfLef+m, intensity, intensity, intensity);
+      }
+    }
+    if (n == 4){
+      for (int i = 0; i < sizeof(num4) / sizeof(num4[0]); i++) {
+        int numberOfLef = num4[i];
+        pixels.setPixelColor(numberOfLef+m, intensity, intensity, intensity);
+      }
+    }
+    if (n == 5){
+      for (int i = 0; i < sizeof(num5) / sizeof(num5[0]); i++) {
+        int numberOfLef = num5[i];
+        pixels.setPixelColor(numberOfLef+m, intensity, intensity, intensity);
+      }
+    }
+    if (n == 6){
+      for (int i = 0; i < sizeof(num6) / sizeof(num6[0]); i++) {
+        int numberOfLef = num6[i];
+        pixels.setPixelColor(numberOfLef+m, intensity, intensity, intensity);
+      }
+    }
+    if (n == 7){
+      for (int i = 0; i < sizeof(num7) / sizeof(num7[0]); i++) {
+        int numberOfLef = num7[i];
+        pixels.setPixelColor(numberOfLef+m, intensity, intensity, intensity);
+      }
+    }
+    if (n == 8){
+      for (int i = 0; i < sizeof(num8) / sizeof(num8[0]); i++) {
+        int numberOfLef = num8[i];
+        pixels.setPixelColor(numberOfLef+m, intensity, intensity, intensity);
+      }
+    }
+    if (n == 9){
+      for (int i = 0; i < sizeof(num9) / sizeof(num9[0]); i++) {
+        int numberOfLef = num9[i];
+        pixels.setPixelColor(numberOfLef+m, intensity, intensity, intensity);
+      }
+    }
+  }
+
+}
+
+void ClearDisplay(){
+  for(int i = 0; i < 63; i++){ 
+    pixels.setPixelColor(i, 0, 0, 0);
+  }
+}
+
+void ConnectWifi(){
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected.");
+}
